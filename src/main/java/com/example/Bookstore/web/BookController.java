d040d95ef97a3fb761a1e26dbc593398f26e1ae1
JKosonen <jussi.k.kosonen@gmail.com>
2022-09-06 18:47:14 +0200

    Add ja Delete

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 
 @Controller
@@ -23,5 +26,23 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+	
+	@RequestMapping(value =("/add"))
+	public String addBook(Model model) {
+		model.addAttribute("books", new Book());
+		return "addbook";
+	}
+	
+	@PostMapping(value ="/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@GetMapping(value="/delete/{id}")
+	public String deleteBook(@PathVariable("id")Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
