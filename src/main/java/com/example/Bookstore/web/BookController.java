5fd7ad08c90fd561e0bf257a74a0076a7cb744d8
JKosonen <jussi.k.kosonen@gmail.com>
2022-09-13 19:16:00 +0200

    koetettu saada validation toimimaan

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,8 +47,8 @@ public class BookController {
 	
 	//save book
 	@PostMapping(value ="/save")
-	public String save(@Valid Book book, BindingResult bindingResult, Model model) {
-		if (bindingResult.hasErrors()) {
+	public String save(@Valid Book book, BindingResult result, Model model) {
+		if (result.hasErrors()) {
 			model.addAttribute("books", book);
 			System.out.println("Joku error tuli, mutta ei näy sivulla");
 			return "addbook";
